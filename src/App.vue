23bc848efb996ad814174ce570666fdded4ab292
Liana Bisuna <liana@thinkbitsolutions.com>
2023-02-22 21:23:53 +0800

    Add primary files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Hello, I have Vite, Vue.js 3, Typescript, Pinia and Tailwind installed!</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
